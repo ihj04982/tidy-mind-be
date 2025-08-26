a88aa219474b9660155071e2d5943472acf94ded
junsu0573 <junsu0573@naver.com>
2025-08-26 21:34:01 +0900

    fix: Fix conflict

--- routes/auth.api.js
+++ routes/auth.api.js
@@ -9,7 +9,11 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 // 구글 로그인
 router.post('/google', authController.googleLogin);
+<<<<<<< HEAD
 // 토큰 검증 - 유저 정보 반환
+=======
+// 토큰 검증
+>>>>>>> 492867f (fix: Fix conflict)
 router.get('/hydrate', authController.authenticate, authController.hydrate);
 
 module.exports = router;
